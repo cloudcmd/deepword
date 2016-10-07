035f3614de0143febf0a43aa15843d2ab0470205
coderaiser <coderaiser@cloudcmd.io>
2016-10-07 18:02:38 +0300

    feature(rollup) add conditional

--- .rollup.js
+++ .rollup.js
@@ -1,30 +1,37 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import builtins from 'rollup-plugin-node-builtins';
+import uglify from 'rollup-plugin-uglify';
+import conditional from 'rollup-plugin-conditional';
 
-import {minify} from 'uglify-js'
+import {minify} from 'uglify-js';
+
+const {NODE_ENV} = process.env;
 
 export default {
     entry: 'client/index.js',
     moduleName: 'deepword',
     plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        }),
         commonjs({
             include: [
+                'client/**',
                 'node_modules/**',
-                'client/**'
             ]
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            runtimeHelpers: true
         }),
         nodeResolve(),
         builtins(),
-        uglify({}, minify),
         postcss({
             extenstions: ['.css']
+        }),
+        conditional({
+            condition: NODE_ENV === 'production',
+            plugin: uglify({}, minify)
         })
     ]
 };
