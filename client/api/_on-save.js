f92b67e39a82ffc9a8ad9d3096c04a47479c8b60
coderaiser <coderaiser@cloudcmd.io>
2017-07-10 14:43:44 +0300

    chore(on-save) rm " "

--- client/api/_on-save.js
+++ client/api/_on-save.js
@@ -26,7 +26,7 @@ export default function _onSave(error, text) {
         this.showMessage(text);
         
         this._story.setData(_filename, _value)
-             .setHash(_filename, this.sha());
+            .setHash(_filename, this.sha());
         
         this.emit('save', _value.length);
     }
