ba9bc23e4bc7eb04bcd5e2dce25eef6f99a7e0d5
coderaiser <coderaiser@cloudcmd.io>
2018-05-15 17:57:15 +0300

    feature(clipboard) add @cloudcmd/clipboard

--- client/api/clipboard.js
+++ client/api/clipboard.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const wraptile = require('wraptile/legacy');
+const clipboard = require('@cloudcmd/clipboard');
+const noop = () => {};
+
 export function copyToClipboard() {
     document.execComand('copy');
     return this;
@@ -12,6 +16,30 @@ export function cutToClipboard() {
 }
 
 export function pastFromClipboard() {
-    this.showMessage('pastFromClipboard: Not implemented');
+    const showMessage = this.showMessage.bind(this);
+    
+    clipboard.readText()
+        .then(paste)
+        .catch(wraptile(showMessage, 'past from clipboard no supported in your browser'));
+    
+    return this;
+}
+
+function paste(text) {
+    const line = editor.getPosition();
+    const range = new monaco.Range(line.lineNumber, 1, line.lineNumber, 1);
+    const identifier = {
+        major: 1,
+        minor: 1,
+    };
+    
+    const op = {
+        identifier,
+         range,
+         text,
+         forceMoveMarkers: true,
+     };
+    
+    editor.executeEdits('deepword', [op]);
 }
 
