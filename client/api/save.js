6d52ced88f3653044bea2a39581f0dec93f18b33
coderaiser <coderaiser@cloudcmd.io>
2018-10-17 16:35:48 +0300

    refactor(save) promise -> async

--- client/api/save.js
+++ client/api/save.js
@@ -1,91 +1,47 @@
 'use strict';
 
-import currify from 'currify/legacy';
-import wraptile from 'wraptile/legacy';
+const wraptile = require('wraptile/legacy');
 
-const ifDiffDo = currify(_ifDiffDo);
-const ifGoodPatch = currify(_ifGoodPatch);
-const ifZipDo = currify(_ifZipDo);
-const ifNotPatchWrite = currify(_ifNotPatchWrite);
-const checkPatch = currify(_checkPatch);
 const setValue = wraptile(_setValue);
 
 export default function() {
-    const doDiff = this._doDiff.bind(this);
-    const _patch = this._patch.bind(this);
-    const value = this.getValue();
-    const {length} = value;
-    const {_filename, _maxSize} = this;
-    
-    const _zip = this._zip.bind(this);
-    const _write = this._write.bind(this);
-    
-    this._loadOptions()
-        .then(ifDiffDo(doDiff, _filename))
-        .then(checkPatch(length, _maxSize))
-        .then(ifGoodPatch(_patch, _filename))
-        .then(ifZipDo(_zip, value))
-        .then(ifNotPatchWrite(_write, _filename))
-        .then(setValue(this))
+    save.call(this)
+        .then(setValue(this));
     
     return this;
 }
 
-function _ifDiffDo(doDiff, value, {diff, zip}) {
-    if (!diff)
-        return {diff, zip};
+async function save() {
+    const value = this.getValue();
+    const {length} = value;
+    const {_filename, _maxSize} = this;
     
-    return doDiff(value)
-        .then((patch) => {
-            return {patch, diff, zip}
-        });
-}
-
-function _checkPatch(length, maxSize, {diff, zip, patch}) {
-    if (!diff)
-        return {zip};
+    const {diff, zip} = await this._loadOptions()
     
-    const patchLength = patch && patch.length || 0;
-    const isLessMaxLength = length < maxSize;
-    const isLessLength = isLessMaxLength && patchLength < length;
-    const isStr = typeof patch === 'string';
-    const isPatch = patch && isStr && isLessLength;
-    
-    return {zip, patch, isPatch};
-}
-
-function _ifGoodPatch(patchFn, filename, {patch, zip, isPatch}) {
-    if (!isPatch)
-        return {zip}
-    
-    return patchFn(filename, patch).then(() => {
-        return {isPatch};
-    });
-}
-
-function _ifZipDo(zipFn, value, {zip, isPatch}) {
-    if (isPatch)
-        return {isPatch};
+    if (diff) {
+        const patch = await this._doDiff(_filename);
+        const isPatch = checkPatch(length, _maxSize, patch);
+        
+        if (isPatch)
+            return this._patch(_filename, patch);
+     }
     
     if (!zip)
-        return {value};
+        return this._write(_filename, value);
     
-    return zipFn(value)
-        .then((value) => {
-            return {zip, value}
-        });
-}
-
-function _ifNotPatchWrite(write, filename, {isPatch, zip, value}) {
-    if (isPatch)
-        return;
-    
-    const uri = !zip ? filename : filename + '?unzip';
-    
-    return write(uri, value);
+    const zipedValue = await this._zip(value);
+    return this._write(`${_filename}?unzip`, zipedValue);
 }
 
 function _setValue(ctx) {
     ctx._value = ctx.getValue();
 }
 
+function checkPatch(length, maxSize, patch) {
+    const patchLength = patch && patch.length || 0;
+    const isLessMaxLength = length < maxSize;
+    const isLessLength = isLessMaxLength && patchLength < length;
+    const isStr = typeof patch === 'string';
+    
+    return patch && isStr && isLessLength;
+}
