e47b52bb726619945775d5b8d6101a6c21f35fa4
coderaiser <coderaiser@cloudcmd.io>
2024-02-02 17:54:21 +0200

    feature: deepword: drop support of node < 18

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
-import {createRequire} from 'node:module';
+import {createRequire} from 'module';
 import path, {dirname} from 'node:path';
 import {fileURLToPath} from 'node:url';
-import process, {env} from 'node:process';
+import {env} from 'node:process';
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const require = createRequire(import.meta.url);
 const dir = './client';
 const isDev = env.NODE_ENV === 'development';
 
 const dist = path.resolve(__dirname, 'dist');
 const distDev = path.resolve(__dirname, 'dist-dev');
 const devtool = isDev ? 'eval' : 'source-map';
+const notEmpty = (a) => a;
+const clean = (array) => array.filter(notEmpty);
+const {resolve} = createRequire(import.meta.url);
 
+const {NODE_ENV} = process.env;
 process.env.NODE_DEBUG = '';
+process.env.NODE_ENV = NODE_ENV || '';
 
-const rules = [{
+const rules = clean([{
     test: /\.js$/,
     exclude: /node_modules/,
     loader: 'babel-loader',
@@ -29,17 +32,14 @@ const rules = [{
         'clean-css-loader',
     ],
 }, {
-    test: /\.(png)$/,
+    test: /\.(png|gif|svg|woff|woff2|eot|ttf)$/,
     use: {
         loader: 'url-loader',
         options: {
             limit: 50_000,
         },
     },
-}, {
-    test: /\.ttf$/,
-    use: ['file-loader'],
-}];
+}]);
 
 export default {
     devtool,
@@ -47,36 +47,34 @@ export default {
         deepword: `${dir}/index.js`,
     },
     output: {
-        library: 'deepword',
+        library: {
+            name: 'deepword',
+            export: 'default',
+            type: 'var',
+        },
         filename: '[name].js',
         path: isDev ? distDev : dist,
         pathinfo: isDev,
-        libraryTarget: 'var',
-        libraryExport: 'default',
         devtoolModuleFilenameTemplate,
     },
     module: {
         rules,
     },
-    resolve: {
+   resolve: {
         fallback: {
-            path: require.resolve('path-browserify'),
-            util: require.resolve('util'),
+            path: resolve('path-browserify'),
+            util: resolve('util'),
         },
-    },
+   },
     plugins: [
-        new webpack.EnvironmentPlugin([
-            'NODE_ENV',
-            'NODE_DEBUG',
-        ]),
-        new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-            resource.request = resource.request.replace(/^node:/, '');
-        }),
-        new MiniCssExtractPlugin(),
+       new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+            resource.request = resource.request.replace(/^node:/, "");
+       }),
+       new webpack.EnvironmentPlugin(['NODE_ENV', 'NODE_DEBUG']),
     ],
 };
 
 function devtoolModuleFilenameTemplate(info) {
     const resource = info.absoluteResourcePath.replace(__dirname + path.sep, '');
-    return `file://deepword/${resource}`;
+    return `file://edward/${resource}`;
 }
